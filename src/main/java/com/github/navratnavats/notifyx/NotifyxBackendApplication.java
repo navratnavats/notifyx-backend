3e6df8f690d6b2fd101308a167e962f7aa9defe0
navratnavats <navratnavats@gmail.com>
2024-12-28 16:20:11 +0530

    initial commit

---
+++ src/main/java/com/github/navratnavats/notifyx/NotifyxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.navratnavats.notifyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/navratnavats/notifyx/NotifyxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.navratnavats.notifyx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
